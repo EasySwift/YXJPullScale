87c96cff88fb2d6e8d60804182cf2bbe5bdc1959
stubbornnessness <1219721814yxj>
2016-11-14 21:55:48 +0800

    3.0.1

--- YXJPullScaleTest/YXJPullScale/YXJPullScale.h
+++ YXJPullScaleTest/YXJPullScale/YXJPullScale.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char YXJPullScaleVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <YXJPullScale/PublicHeader.h>
 
 
+#import "UIScrollView+PullScale.h"
